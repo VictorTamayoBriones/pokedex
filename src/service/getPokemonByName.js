88ed34b534f67f612bbce20de7c4f6a6732a12d8
Víctor Tamayo <victortamayo509@gmail.com>
2022-07-23 17:38:59 -0500

    toast message err

--- src/service/getPokemonByName.js
+++ src/service/getPokemonByName.js
@@ -1,6 +1,15 @@
 import axios from "axios";
 
 export const getPokemonByName = async (name) =>{
-    const res = await axios(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    return res.data;
+    try{
+        const res = await axios(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        return res.data;
+    }catch(err){
+        let e = err.response.data
+        console.log(e);
+        new Toast({
+            message: e,
+            type: 'danger'
+        });
+    }
 }
