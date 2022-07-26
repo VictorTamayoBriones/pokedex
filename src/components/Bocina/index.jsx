47905c56f99d32553cb26860fbc76c8098efa470
Víctor Tamayo <victortamayo509@gmail.com>
2022-07-25 22:16:28 -0500

    speak abilities

--- src/components/Bocina/index.jsx
+++ src/components/Bocina/index.jsx
@@ -3,21 +3,18 @@ import { useContext, useEffect, useState } from "react"
 import { CurrentPokemonContext } from '../../context/currentPokemon';
 
 export const Bocina = () =>{
-    const { currentPokemon } = useContext(CurrentPokemonContext)
-    // const [abilities, setAbilities]=useState();
-    
+    const { currentPokemon:{abilities, name} } = useContext(CurrentPokemonContext)
 
     const msg = new SpeechSynthesisUtterance();
-    msg.text = `El es ${currentPokemon.name}, Habilidades pendientes`;
-
+    msg.text = `El es ${name}, Habilidades: ${abilities?.map(a=>a.ability.name)}`;
+    
+    
     useEffect(()=>{
-        if( currentPokemon.name != undefined ){
-            // currentPokemon.abilities.forEach(a=>{
-            //     setAbilities([...abilities, a.ability.name]);
-            // })        
+        if( name != undefined ){
             window.speechSynthesis.speak(msg);
         }
-    }, [currentPokemon.name]);
+    }, [name])
+
     return(
         <Speaker>
 
