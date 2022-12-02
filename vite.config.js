a840721121b885d98588bf408da6949507ecdbdf
Victor Mnauel Tamayo Briones <victor@extjs.mx>
2022-12-02 14:23:40 -0600

    config manifest

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,27 @@ export default defineConfig({
       registerType: 'autoUpdate',
       devOptions: {
         enabled: true
+      },
+      includeAssets: ['logo.svg'],
+      manifest: {
+        name: 'My Pokedex',
+        short_name: 'Pokedex',
+        description: 'This my awaesome pokedez and ir can talk!!!',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo.png',
+            sizes: '192x192',
+            type: 'image/svg'
+          },
+          {
+            src: 'logo.svg',
+            sizes: '512x512',
+            type: 'image/svg'
+          }
+        ]
       }
+      
     })
   ],
   server: {
